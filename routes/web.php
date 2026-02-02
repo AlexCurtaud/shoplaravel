d0fd42469aaf5a178b49ae44ad9cdcdca54dd6f3
Alex Curtaud <alex.curtaud@gmail.com>
2026-02-02 16:01:06 +0100

    Partie 1 - Bonus

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductController;
@@ -22,4 +24,19 @@ Route::get('/products', [ProductController::class, 'index'])
     ->name('products.index');
 
 Route::get('/products/{id}', [ProductController::class, 'show'])
-    ->name('products.show');
+    ->name('products.show')
+    ->whereNumber('id');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])
+        ->name('admin.dashboard');
+    Route::get('/users', [AdminController::class, 'usersList'])
+        ->name('usersList');
+});
+
+Route::resource('category', CategoryController::class);
+
+// Route Fall Back
+Route::fallback(function() {
+    return 'Erreur 404 - Page non trouvée ! <a href="/">Retour à l\'acceuil</a>';
+});
